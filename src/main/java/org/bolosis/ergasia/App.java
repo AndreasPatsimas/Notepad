a3d7b0a9038e652ea51657092ba8f45d77787e4a
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-06-12 11:53:58 +0300

    Update App.java

--- src/main/java/org/bolosis/ergasia/App.java
+++ src/main/java/org/bolosis/ergasia/App.java
@@ -1,9 +1,13 @@
 package org.bolosis.ergasia;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 public class App 
 {
-    public static void main( String[] args ) 
+    public static void main( String[] args ) throws UnknownHostException 
     {
+    	System.out.println(Inet4Address.getLocalHost().getHostAddress());
     	new TextEditor();  
     	
     }
